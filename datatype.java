4dd50f01399a251cb3433d1ae45a76e3341adad8
frostt9406 <602nimbus@gmail.com>
2025-03-22 23:15:56 +0530

    day1

---
+++ datatype.java
@@ -0,0 +1,21 @@
+package javatraining;
+
+public class datatype {
+
+	public static void main(String[] args) {
+		String name="nayab nadeem siddiqui";
+		byte age= 21;
+		long numberphone= 9810988454l;//have to put l otherwise default take int
+		char gender='f';
+		
+		
+		System.out.println(name);
+		System.out.println(age);
+		System.out.println(name);
+		System.out.println(name);
+		
+		
+
+	}
+
+}
