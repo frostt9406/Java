8fe095966af27b22abf8beb6766e6f87a5a4ab41
frostt9406 <602nimbus@gmail.com>
2025-03-22 23:20:12 +0530

    day2

---
+++ Methoddefine.java
@@ -0,0 +1,15 @@
+package javatraining;
+
+public class Methoddefine {
+
+	public static void main(String[] args) {
+		add(); //method calling
+
+	}
+	public static void add() {
+		int num1=10;
+		int num2=20;
+		System.out.println(num1+num2);
+	}
+
+}
---
+++ Uppercasecheck.java
@@ -0,0 +1,12 @@
+package javatraining;
+
+public class Uppercasecheck {
+
+	public static void main(String[] args) {
+		char ch='N';
+		//constraint= no special characters included
+		System.out.println((ch>=97&&ch<=122)?"lowercase":"uppercase");//a-z is 97 to 122
+
+	}
+
+}
